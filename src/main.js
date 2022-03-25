57010be4f9f0244962bb613972b94f0ad3253484
arnav-kr <72879799+arnav-kr@users.noreply.github.com>
2022-03-25 22:13:50 +0530

    Remove count sadly

--- src/main.js
+++ src/main.js
@@ -12,49 +12,32 @@ import './css/style.css';
 // });
 //
 // PWAManagerInstance.init();
-import {
-  AbsoluteOrientationSensor,
-  RelativeOrientationSensor
-} from './js/polyfills/motion-sensor.js';
-import ToEulerAngles from './js/utils/toEulerAngles';
 
-const params = new URLSearchParams(new URL(window.location.href).search.slice(1));
-const relative = false
-const coordinateSystem = params.get("coord");
-const colors = {
-  red: "#ef4444",
-  green: "#22c55e",
-}
-let color = colors.green;
+let mode = "like";
 let icon = document.getElementById('icon');
 let btn = document.getElementById('btn');
-let likeCount = document.getElementById('like-count');
-let reactionWrapper = document.getElementById('reaction-wrapper');
-let transform = 0, opacity = 1, mode = "like";
-let reactions = { likes: 0, dislikes: 0 };
 btn.addEventListener('click', () => {
-  icon.style.fill = color;
+  if (mode === "like") {
+    console.log("Like");
+    icon.classList.add("!fill-green-500");
+    icon.classList.remove("!fill-red-500");
+    setTimeout(() => {
+      icon.classList.remove("!fill-green-500");
+      icon.classList.remove("!fill-red-500");
+    }
+      , 1000);
+  }
+  if (mode === "dislike") {
+    console.log("Dislike");
+    icon.classList.add("!fill-red-500");
+    icon.classList.remove("!fill-green-500");
+    setTimeout(() => {
+      icon.classList.remove("!fill-green-500");
+      icon.classList.remove("!fill-red-500");
+    }
+      , 1000);
+  }
 });
-
-// if (navigator.permissions) {
-//   // https://w3c.github.io/orientation-sensor/#model
-//   Promise.all([navigator.permissions.query({ name: "accelerometer" }),
-//   navigator.permissions.query({ name: "magnetometer" }),
-//   navigator.permissions.query({ name: "gyroscope" })])
-//     .then(results => {
-//       if (results.every(result => result.state === "granted")) {
-//         initSensor();
-//       } else {
-//         console.log("Permission to use sensor was denied.");
-//       }
-//     }).catch(err => {
-//       console.log("Integration with Permissions API is not enabled, still try to start app.");
-//       initSensor();
-//     });
-// } else {
-//   console.log("No Permissions API, still try to start app.");
-//   initSensor();
-// }
 
 if (window.DeviceOrientationEvent) {
   initSensor();
@@ -64,34 +47,14 @@ else {
 }
 
 function initSensor() {
-  window.addEventListener('deviceorientation', function (event) {
+  window.addEventListener('deviceorientation', (event) => {
     let x = event.beta;
-    let y = event.gamma;
     let z = event.alpha;
-    let threshold = 20;
     if (x >= 0 || x >= 0) {
-      console.log("Up");
       mode = "like";
-      color = colors.green;
     }
     if (x < 0 || z < 0) {
-      console.log("Down");
       mode = "dislike";
-      color = colors.red;
     }
-    transform = z;
   });
 }
-
-function animate() {
-  requestAnimationFrame(animate);
-  reactionWrapper.style.transform = `rotate(${transform}deg)`;
-  reactionWrapper.style.opacity = opacity;
-  if (mode == "like") {
-    likeCount.innerText = `+${reactions.likes}`;
-  }
-  else {
-    likeCount.innerText = `-${reactions.dislikes}`;
-  }
-}
-requestAnimationFrame(animate);
